b9c39e017a50f95b427a6dec9f48d63ae6f056ce
ultraprogrammer <ultraprogrammer13@gmail.com>
2018-10-09 19:49:18 -0400

    Update fibonacci.py

--- Numbers/fibonacci.py
+++ Numbers/fibonacci.py
@@ -1,16 +1,25 @@
 """
 Fibonacci sequence generator
 """
-def fib(n):
+def fib(limit):
     '''
-    Generate a fibonacci sequence up to n
+    Generate a fibonacci sequence up to limit
     '''
-    a = 1
-    b = 1
-    for i in range(n):
-        yield a
-        a,b = b,a+b
+    num_1 = 1
+    num_2 = 1
+    for i in range(limit):
+        yield num_1
+        num_1, num_2 = num_2, num_1 + num_2
 
 #Test
-for i in fib(10):
-    print(i)
+while True:
+    try:
+        FIB_LIMIT = int(input('Print all fibonacci numbers up to: '))
+    except ValueError:
+        print('I\'m sorry! You must enter an integer! Please try again!\n')
+        continue
+    else:
+        break
+
+for num in fib(FIB_LIMIT):
+    print(num)
