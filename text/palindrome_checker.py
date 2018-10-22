0dadd5d389ba7633a6b97f9625cb350955dad619
ultraprogrammer <ultraprogrammer13@gmail.com>
2018-10-21 21:25:16 -0400

    Create palindrome_checker.py

---
+++ text/palindrome_checker.py
@@ -0,0 +1,38 @@
+"""
+Check if a phrase is a palindrome
+"""
+def check_palindrome(input_string):
+    '''
+    Palindrome checker
+    '''
+    reversed_words = []
+    if len(input_string.split()) == 1:
+        return input_string[::-1] == input_string
+    else:
+        for word in input_string.split():
+            reversed_words.append(word[::-1])
+
+        return ''.join(reversed_words[::-1]) == ''.join(input_string.split())
+
+while True:
+    try:
+        STRING = input('Phrase to be checked for palindrome: ')
+        if len(STRING) < 1:
+            raise RuntimeError
+    except RuntimeError:
+        print('Please enter a phrase to be checked for palindrome!\n')
+        continue
+    else:
+        break
+
+def main():
+    '''
+    Wrapper function
+    '''
+    if check_palindrome(STRING):
+        print(f'{STRING.capitalize()} is a palindrome.')
+    else:
+        print(f'{STRING.capitalize()} is not a palindrome.')
+
+if __name__ == '__main__':
+    main()
